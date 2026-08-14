cc6121975b8f63ffa49e98620c8c1cb7e96b1da0
Rajveer Bijarnia <rajveerbijarnia115853@gmail.com>
2026-08-14 10:10:22 +0530

    ✅ Two Sum | Easy | C++ | Runtime: 43 ms (24.43%) | Memory: 14.2 MB | 2026-08-14

--- leetcode/easy/0001-two-sum/solution.cpp
+++ leetcode/easy/0001-two-sum/solution.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> num_map;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
-            
-            if (num_map.find(complement) != num_map.end()) {
-                return {num_map[complement], i};
+        int i;
+        int j;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
             }
-            
-            num_map[nums[i]] = i;
-        }
-        
-        return {};
+        }return {};
     }
 };
